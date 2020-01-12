4147ac6fdc07a545e06fb662736e120e9e29f98d
jaylamrin <jaylamrin@gmail.com>
2020-01-11 19:13:09 -0700

    Removed imports

--- test.py
+++ test.py
@@ -1,14 +1,9 @@
-import math
-import os
-import sys
-
 import requests
 
-print(sys.version)
-print(sys.executable)
 
 r = requests.get("https://rmotr.com")
 print(r.status_code)
+print(r.ok)
 
 name = input("Your name? ")
 print(f"Hello {name}")
